43ad70eb5caad46299220b5efad1f4b58b36cf0e
fardean <a@g.com>
2025-12-17 17:06:00 +0330

    print to stderr

--- src/main.rs
+++ src/main.rs
@@ -46,13 +46,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments: {err}");
+        eprintln!("problem parsing arguments: {err}");
         process::exit(1);
     });
     println!("Searching for \"{}\" in file {}", config.query, config.file_path);
 
     if let Err(e) = run(&config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
